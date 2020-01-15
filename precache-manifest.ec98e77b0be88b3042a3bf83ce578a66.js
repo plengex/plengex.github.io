a639f859143b95beb54d80cdecd2447fbdcc604b
Wildanun Nabil <plengex09@gmail.com>
2020-01-15 18:19:32 +0700

    initial commit

---
+++ precache-manifest.ec98e77b0be88b3042a3bf83ce578a66.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2259f8015619100069810ac063b2c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e1c46e4f280fe7b939a",
+    "url": "/static/css/main.95e0b630.chunk.css"
+  },
+  {
+    "revision": "605610e610bcf0992f11",
+    "url": "/static/js/2.5cef9066.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5cef9066.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e1c46e4f280fe7b939a",
+    "url": "/static/js/main.d7aa4a2c.chunk.js"
+  },
+  {
+    "revision": "f1341830b714019e362e",
+    "url": "/static/js/runtime-main.f6e7cd38.js"
+  },
+  {
+    "revision": "df812b7ceef3863847c66d4f87f1e93c",
+    "url": "/static/media/app-icon.df812b7c.png"
+  },
+  {
+    "revision": "2c5f464f97da108badffae0d4c6f48d8",
+    "url": "/static/media/back-icon.2c5f464f.png"
+  },
+  {
+    "revision": "a7c3d557de0ef24c46e9a31befda0b02",
+    "url": "/static/media/biodata.a7c3d557.png"
+  },
+  {
+    "revision": "29d117920b0c69bc853c37b345bacb52",
+    "url": "/static/media/logo.29d11792.png"
+  },
+  {
+    "revision": "613878d502df2b2c394b333bb66d3e0b",
+    "url": "/static/media/penghasilan.613878d5.png"
+  },
+  {
+    "revision": "221cbf5474ddea22b198819c1ccd6cdc",
+    "url": "/static/media/perhiasan.221cbf54.png"
+  },
+  {
+    "revision": "240c271cfa6085d7448b8283980d8c29",
+    "url": "/static/media/perniagaan.240c271c.svg"
+  },
+  {
+    "revision": "fd5a5dc3cb69d9fc27bdb8f439e5706f",
+    "url": "/static/media/pertanian.fd5a5dc3.png"
+  },
+  {
+    "revision": "8b8f8e35d8a9c3de914ba49c41f0e13b",
+    "url": "/static/media/rikaz.8b8f8e35.png"
+  },
+  {
+    "revision": "1811dfd3b7720d59688d0bcfd27072e6",
+    "url": "/static/media/ternak.1811dfd3.png"
+  }
+]);
